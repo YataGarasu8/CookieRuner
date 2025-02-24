0b208474bdcb7cdfb910f1ee9206c03ee994b0f8
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-24 14:33:34 +0900

    [Update] 임시

--- Assets/02. Scripts/04. Scene/03.GameScene/HPController.cs
+++ Assets/02. Scripts/04. Scene/03.GameScene/HPController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HPCotroller : MonoBehaviour
 {
     public PlayerStatsData statsData;
     PlayerStatsData runTimeData;
+
+    public Slider Slider;
+    Image fillArea;
+
 
     private void Awake()
     {
