7c0eee5b9261b9b4df262b99b2d0752c23e136c4
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-27 11:22:29 +0900

    [Update]랭킹보드 활성화

--- Assets/02. Scripts/03. Scene/03.GameScene/GameUIController.cs
+++ Assets/02. Scripts/03. Scene/03.GameScene/GameUIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 using UnityEngine.SceneManagement;
 
 public class GameUIController : MonoBehaviour
@@ -10,14 +11,16 @@ public class GameUIController : MonoBehaviour
     public TextMeshProUGUI CoinCountText;
     public GameObject PausePanel;
     public GameObject EndPanel;
+    public GameObject RankingBoard;
     public GameObject Canvas;
 
-
+    bool isMove = false;
 
     private void Awake()
     {
         PausePanel.gameObject.SetActive(false);
         EndPanel.gameObject.SetActive(false);
+        RankingBoard.gameObject.SetActive(false);
     }
     private void Start()
     {
@@ -32,29 +35,49 @@ public class GameUIController : MonoBehaviour
 
         CoinCountText.text = GameManager.Instance.Money.ToString();
 
-        if(GameManager.Instance.IsGameOver == true)
+        if (GameManager.Instance.IsGameOver == true)
         {
-            EndPanel.gameObject.SetActive(true);
+            GameOver();
         }
     }
 
     public void PauseGame()
     {
-            PausePanel.gameObject.SetActive(true);
-            Time.timeScale = 0f;
+        PausePanel.gameObject.SetActive(true);
+        Time.timeScale = 0f;
     }
     public void ReturnGame()
     {
-            PausePanel.gameObject.SetActive(false);
-            Time.timeScale = 1f;
+        PausePanel.gameObject.SetActive(false);
+        Time.timeScale = 1f;
     }
     public void OutGame()
     {
         PausePanel.gameObject.SetActive(false);
-        Canvas.gameObject.SetActive(false);
         EndPanel.gameObject.SetActive(false);
+        RankingBoard.gameObject.SetActive(false);
+        Canvas.gameObject.SetActive(false);
+        SoundManager.Instance.StopBGM();
         SceneManager.LoadScene("LobbyScene");
         Time.timeScale = 1f;
     }
-
+    public void GameOver()
+    {
+        EndPanel.gameObject.SetActive(true);
+        Invoke("SetRankingBoard", 2.2f);
+    }
+    public void SetRankingBoard()
+    {
+        if (!isMove)
+        {
+            if (!RankingBoard.activeSelf)
+            {
+                RankingBoard.gameObject.SetActive(true);
+                RankingBoard.transform.DOLocalMoveY(0, 0.5f).SetEase(Ease.OutExpo).OnComplete
+                    (
+                    () => { isMove = false; }
+                    );
+            }
+        }
+    }
 }
