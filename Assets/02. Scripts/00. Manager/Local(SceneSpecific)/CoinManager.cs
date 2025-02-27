e8372452f6ae52dc799471767f9cacb484fe6f90
rlck339 <a95895899@gmail.com>
2025-02-27 15:17:00 +0900

    .
    .

--- Assets/02. Scripts/00. Manager/Local(SceneSpecific)/CoinManager.cs
+++ Assets/02. Scripts/00. Manager/Local(SceneSpecific)/CoinManager.cs
@@ -9,7 +9,6 @@ public class CoinManager : MonoBehaviour
     public Text addGold;
     public TextMeshProUGUI getGold;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +20,7 @@ public class CoinManager : MonoBehaviour
     {
         
     }
-    void WriteMoney()
+    public void WriteMoney()
     {
         if (addGold != null)
         {
